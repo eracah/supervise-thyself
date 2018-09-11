3518e9bdb63823a543bdb5c5d46f30d1d3d173b8
eracah <ejracah@gmail.com>
2018-09-11 18:09:47 -0400

    added 100x100 grid

--- data/custom_grids.py
+++ data/custom_grids.py
@@ -1,36 +1,24 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[2]:
 
 
 from gym_minigrid.envs import EmptyEnv
 
 
-# In[6]:
+# In[3]:
 
 
 from gym_minigrid.register import register
 
 
-# In[7]:
+# In[4]:
 
 
 class EmptyEnv32x32(EmptyEnv):
     def __init__(self):
         super().__init__(size=32)
-
-
-# In[ ]:
-
-
-class EmptyEnv64x64(EmptyEnv):
-    def __init__(self):
-        super().__init__(size=64)
-
-
-# In[8]:
-
 
 register(
     id='MiniGrid-Empty-32x32-v0',
@@ -38,11 +26,28 @@ register(
 )
 
 
-# In[ ]:
+# In[5]:
 
+
+class EmptyEnv64x64(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=64)
 
 register(
     id='MiniGrid-Empty-64x64-v0',
     entry_point='data.custom_grids:EmptyEnv64x64'
+)
+
+
+# In[6]:
+
+
+class EmptyEnv100x100(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=100)
+
+register(
+    id='MiniGrid-Empty-100x100-v0',
+    entry_point='data.custom_grids:EmptyEnv100x100'
 )
 
