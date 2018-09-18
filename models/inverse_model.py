0f94e90ba17b82884d9620b1f4ae63a85c9a7545
eracah <ejracah@gmail.com>
2018-09-18 15:29:09 -0400

    moved construction of encoder object to inside constructor of inverse model

--- models/inverse_model.py
+++ models/inverse_model.py
@@ -1,16 +1,7 @@
-
-# coding: utf-8
-
-# In[10]:
-
-
 import torch
 from torch import nn
 import torch.functional as F
-
-
-# In[11]:
-
+from models.base_encoder import Encoder
 
 class ActionPredictor(nn.Module):
     def __init__(self, num_actions, in_ch, h_ch=256):
@@ -25,14 +16,16 @@ class ActionPredictor(nn.Module):
         
 
 class InverseModel(nn.Module):
-    def __init__(self,encoder, num_actions=3):
+    def __init__(self, in_ch=3, im_wh=(64,64), h_ch=32, embed_len=32, batch_norm=False, num_actions=3):
         super(InverseModel,self).__init__()
-        self.encoder = encoder
-        self.embed_len = self.encoder.embed_len
-        self.ap = ActionPredictor(num_actions=num_actions,in_ch=2*self.embed_len)
+        self.encoder = Encoder(in_ch=in_ch, im_wh=im_wh, h_ch=h_ch, embed_len=embed_len, batch_norm=batch_norm)
+        self.ap = ActionPredictor(num_actions=num_actions,in_ch=2*self.encoder.embed_len)
+    
     def forward(self,x0,x1):
         f0 = self.encoder(x0)
         f1 = self.encoder(x1)
         fboth = torch.cat([f0,f1],dim=-1)
         return self.ap(fboth)
-
+    
+    def loss(self,pred, true):
+        return nn.CrossEntropyLoss()(pred,true)
