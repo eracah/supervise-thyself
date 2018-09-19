8e1160f632db6e6efb313e0fcc766ca4afa824c9
eracah <ejracah@gmail.com>
2018-09-19 16:41:12 -0400

    made training loop general to inv_model and vae family

--- train_inv_model.py
+++ train_inv_model.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 import random
@@ -14,6 +14,7 @@ from utils import setup_env
 from data.replay_buffer import BufferFiller
 import argparse
 from models.inverse_model import InverseModel
+from models.baselines import VAE, BetaVAE
 from utils import convert_frame, classification_acc, mkstr, setup_dirs_logs, parse_minigrid_env_name
 import argparse
 import sys
@@ -47,16 +48,18 @@ def setup_args(test_notebook):
     parser.add_argument("--hidden_width",type=int,default=32)
     parser.add_argument("--embed_len",type=int,default=32)
     parser.add_argument("--seed",type=int,default=4)
+    parser.add_argument("--model",type=str,default="vae")
+    parser.add_argument("--beta",type=float,default=1.0)
     args = parser.parse_args()
     args.resize_to = tuple(args.resize_to)
 
     sys.argv = tmp_argv
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
     mstr = partial(mkstr,args=args)
-    output_dirname = ("nb_" if test_notebook else "") + "_".join(["inv_model_e%s"%parse_minigrid_env_name(args.env_name),
-                                                                        "r%i"%(args.resize_to[0])
-                                                                       ])
-    args.output_dirname = output_dirname
+#     output_dirname = ("nb_" if test_notebook else "") + "_".join(["%s_e%s"%(args.model,parse_minigrid_env_name(args.env_name)),
+#                                                                         "r%i"%(args.resize_to[0])
+#                                                                        ])
+#     args.output_dirname = output_dirname
     return args
 
 
@@ -70,7 +73,7 @@ class Trainer(object):
         self.tr_buf =tr_buf
         self.args = args
         self.model_dir = model_dir
-
+        self.model_name = self.args.model
         self.writer=writer
         
         #self.opt_template = partial(Adam,params=self.model.parameters())
@@ -83,12 +86,16 @@ class Trainer(object):
     def one_iter(self, trans, update_weights=True):
         if update_weights:
             self.opt.zero_grad()
-        pred = self.model(trans.x0,trans.x1)
-        true = trans.a
-        loss =  nn.CrossEntropyLoss()(pred,true)
-            
-            
-        acc = classification_acc(logits=pred,true=true)
+        if self.model_name == "inv_model":
+            pred = self.model(trans.x0,trans.x1)
+            true = trans.a
+            loss =  self.model.loss(pred,true)
+            acc = classification_acc(logits=pred,true=true)
+        elif self.model_name == "vae" or self.model_name == "beta_vae":
+            pred = self.model(trans.x0)
+            true = trans.x0
+            loss = self.model.loss(true, *pred)
+            acc = 0.0
         if update_weights:
             loss.backward()
             self.opt.step()
@@ -104,8 +111,8 @@ class Trainer(object):
         
         avg_loss = np.mean(losses)
         avg_acc = np.mean(accs)
-        writer.add_scalar("inv_model/%s_loss"%mode,avg_loss,global_step=self.epoch)
-        writer.add_scalar("inv_model/%s_acc"%mode,avg_acc,global_step=self.epoch)
+        writer.add_scalar("%s/%s_loss"%(self.model_name, mode), avg_loss, global_step=self.epoch)
+        writer.add_scalar("%s/%s_acc"%(self.model_name,mode), avg_acc, global_step=self.epoch)
         if mode == "train":
             print("Epoch %i: "%self.epoch)
         print("\t%s"%mode)
@@ -114,7 +121,6 @@ class Trainer(object):
     
         
     def train(self):
-        #self.opt = self.opt_template(lr=lr)
         state_dict = self.model.state_dict()
         val_acc = -np.inf
         best_val_loss = np.inf
@@ -127,35 +133,29 @@ class Trainer(object):
             torch.save(self.model.state_dict(), self.model_dir / "cur_model.pt")
             self.model.eval()
             val_loss, val_acc = self.one_epoch(self.val_buf,mode="val")
+            if self.epoch == 1:
+                torch.save(self.model.state_dict(), self.model_dir / Path(  ("best_model_%f.pt"%val_loss).rstrip('0').rstrip('.')))
             if val_loss < best_val_loss:
                 best_val_loss = copy.deepcopy(val_loss)
                 for f in self.model_dir.glob("best_model*"):
                     os.remove(str(f))
                 torch.save(self.model.state_dict(), self.model_dir / Path(  ("best_model_%f.pt"%best_val_loss).rstrip('0').rstrip('.')))
 
-def split_tr_set(tr_buf, fraction=0.8):
-    t1 = time.time()
-    print("doing split")
-    tr_buf,val_buf = bf.split(tr_buf,fraction)
-    t2 = time.time()
-    print("%8.4f seconds"%(t2-t1))
-    
-
-    return tr_buf, val_buf
 
 def setup_model(args, action_space):
-    encoder = Encoder(in_ch=3,
-    im_wh=args.resize_to,
-    h_ch=args.hidden_width,
-    embed_len=args.embed_len).to(args.device)
+    model_table = {"inv_model":InverseModel, "vae":VAE, "beta_vae": BetaVAE}
+    encoder_kwargs = dict(in_ch=3,im_wh=args.resize_to,h_ch=args.hidden_width, embed_len=args.embed_len,  
+                        num_actions=len(action_space), beta=args.beta)
 
-    inv_model = InverseModel(encoder=encoder,num_actions=len(action_space)).to(args.device)
-    return inv_model    
+    model = model_table[args.model](**encoder_kwargs ).to(args.device)
+    return model    
 
 def setup_exp_name(test_notebook, args):
     prefix = ("nb_" if test_notebook else "")
     exp_name = Path(prefix  + "_".join([ ("lr%f"%args.lr).rstrip('0').rstrip('.'),"%s"%parse_minigrid_env_name(args.env_name), "r%i"%(args.resize_to[0])]))
-    base_dir = Path("inv_model")
+    if args.model == "beta_vae":
+        exp_name = Path(str(exp_name) + ("_%f"%args.beta).rstrip('0').rstrip('.'))
+    base_dir = Path(args.model)
     return base_dir / exp_name
     
 
@@ -172,20 +172,16 @@ if __name__ == "__main__":
     exp_name = setup_exp_name(test_notebook,args)
     writer, model_dir = setup_dirs_logs(args,exp_name)
     env, action_space, grid_size, num_directions, tot_examples, random_policy = setup_env(args.env_name,args.seed)
-    inv_model = setup_model(args, action_space)
+    model = setup_model(args, action_space)
+    
+    
 
     print("starting to load buffers")
     tr_buf, val_buf = setup_tr_val_val_test(env, random_policy, 
-                                convert_fxn, tot_examples,args.batch_size,just_train=True)
+                                 convert_fxn, tot_examples,args.batch_size,just_train=True)
     print("done loading buffers")
     
 
-    trainer = Trainer(inv_model, tr_buf, val_buf, model_dir, args, writer)
+    trainer = Trainer(model, tr_buf, val_buf, model_dir, args, writer)
     trainer.train()
-
-
-# In[ ]:
-
-
-
 
