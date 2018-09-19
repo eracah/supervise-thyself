70c05c1a7764a0bb4c1fd88110cc0db716935cfd
eracah <ejracah@gmail.com>
2018-09-19 16:32:28 -0400

    removed is_vae option and just store the pre-embedding hidden units, so they can be used by the vae

--- models/base_encoder.py
+++ models/base_encoder.py
@@ -33,14 +33,14 @@ from functools import partial
 class Encoder(nn.Module):
     def __init__(self,im_wh=(64,64),in_ch=3,
                  h_ch=32,embed_len=32, 
-                 batch_norm=False, is_vae=False):
+                 batch_norm=False):
         super(Encoder,self).__init__()
         self.bias= False if batch_norm else True
         self.im_wh = im_wh 
         self.in_ch = in_ch
         self.h_ch = h_ch
-        self.is_vae = is_vae
         self.embed_len = embed_len
+        self.vec = None
         layers = [nn.Conv2d(in_channels=self.in_ch, out_channels=self.h_ch,
                       kernel_size=3, stride=2, padding=1,bias=self.bias),
             nn.BatchNorm2d(self.h_ch),
@@ -67,10 +67,8 @@ class Encoder(nn.Module):
                     
         self.fc = nn.Linear(in_features=self.enc_out_shape,
                             out_features=self.embed_len)
-        if self.is_vae:
-            self.logvar_fc = nn.Linear(in_features=self.enc_out_shape,
-                            out_features=self.embed_len)
-            
+
+
 
     @property
     def enc_out_shape(self):
@@ -82,91 +80,9 @@ class Encoder(nn.Module):
         a = torch.randn(inp_shape)
         return self.encoder(a).size()[1:]
     
-#     def get_feature_maps(self):
-#         return self.fmaps
 
     def forward(self,x):
         fmaps = self.encoder(x)
-        vec = fmaps.view(fmaps.size(0),-1)
-        if self.is_vae and self.training:
-            mu = self.fc(vec)
-            logvar = self.logvar_fc(vec)
-            return mu, logvar
-        else:
-            embedding = self.fc(vec)
-            return embedding
-
-
-# In[3]:
-
-
-# if __name__ == "__main__":
-#     rpe = RawPixelsEncoder()
-#     x = torch.randn((10,3,64,64))
-#     f = rpe(x)
-#     print(f.size())
-    
-
-
-# In[2]:
-
-
-# if __name__ == "__main__":
-#     x = torch.randn((10,3,64,64))
-#     x0 = x[0][None,:]
-#     enc = Encoder(embed_len=32)
-#     q = QNet(enc)
-#     print(q(x0))
-#     print(list(dict(q.named_parameters()).keys()))
-    
-
-
-# In[1]:
-
-
-# if __name__ == "__main__":
-#     x = torch.randn((10,3,64,64))
-#     enc = Encoder(embed_len=64)
-#     print(enc(x).size())
-if __name__ == "__main__":
-    x0 = torch.randn((10,3,64,64))
-    x1 = torch.randn((10,3,64,64))
-    enc = Encoder(embed_len=64)
-    im = InverseModel(encoder=enc)
-
-    print(im(x0,x1).size())
-    print(list(dict(im.named_parameters()).keys()))
-
-# enc = Encoder(batch_norm=True)
-
-# x = torch.randn(8,3,64,64)
-
-# vec = enc(x)
-
-# print(vec.size())
-# print(enc.get_output_shape((8,3,64,64)))
-
-# enc = Encoder(batch_norm=True)
-
-# x1 = torch.randn(8,3,64,64)
-# x2 = torch.randn(8,3,64,64)
-# vec1 = enc(x1)
-# vec2 = enc(x2)
-# vec = torch.cat((vec1,vec2),dim=-1)
-# ap = ActionPredictor(3,1024)
-
-# logits = ap(vec)
-# print(logits.size())
-
-# prd = InverseModel(in_ch=3,im_wh=(64,64),h_ch=32,num_actions=4,batch_norm=False)
-
-# x1 = torch.randn(8,3,64,64)
-# x2 = torch.randn(8,3,64,64)
-
-# prd(x1,x2)
-
-# prd.parameters()
-
-# nn.CrossEntropyLoss?
-
-
+        self.vec = fmaps.view(fmaps.size(0),-1)
+        embedding = self.fc(self.vec)
+        return embedding
