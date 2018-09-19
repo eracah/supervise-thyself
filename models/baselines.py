d55d719fc991cc9446f2fceb289dded12ab41458
eracah <ejracah@gmail.com>
2018-09-19 16:35:51 -0400

    normalize both loss terms by num_pixels. also moved fc layer for inferring logvar to vae and out of encoder object

--- models/baselines.py
+++ models/baselines.py
@@ -58,10 +58,9 @@ class RandomWeightCNN(Encoder):
 class Decoder(nn.Module):
     def __init__(self,encoder):
         super(Decoder,self).__init__()
-        self.enc = encoder
-        
-        
-        layers = self.setup_decoder_layers()
+        self.enc_dict =  dict(encoder.named_modules())
+        self.lsh = encoder.last_im_shape
+        layers = self.setup_decoder_layers(encoder)
         self.decoder = nn.Sequential(*layers)
         
         enc_fc = self.enc_dict["fc"]
@@ -69,16 +68,20 @@ class Decoder(nn.Module):
                             out_features=enc_fc.in_features)
 
 
-    def setup_decoder_layers(self):
-        bias = self.enc.bias
-        self.enc_dict =  dict(self.enc.named_modules())
+    def setup_decoder_layers(self,encoder):
+        bias = encoder.bias
+
         enc_list = [self.enc_dict[key] for key in self.enc_dict.keys() if "encoder." in key ]
         enc_list.reverse()
         layers = []
         for lay in enc_list:
             if "Conv" in str(lay):
-                layer = nn.ConvTranspose2d(in_channels=lay.out_channels, out_channels=lay.in_channels,
-                      kernel_size=lay.kernel_size, stride=lay.stride, padding=lay.padding,bias=bias,
+                layer = nn.ConvTranspose2d(in_channels=lay.out_channels,
+                                           out_channels=lay.in_channels,
+                                           kernel_size=lay.kernel_size,
+                                           stride=lay.stride,
+                                           padding=lay.padding,
+                                           bias=bias,
                                            output_padding=1)
             else:
                 layer = copy.deepcopy(lay)
@@ -87,8 +90,8 @@ class Decoder(nn.Module):
         
     def forward(self,h):
         ht = self.fc(h)
-        lch,lh,lw = self.enc.last_im_shape
-        h_im = ht.view(-1, lch, lh, lw)
+        
+        h_im = ht.view(-1, *self.lsh)
         im = self.decoder(h_im)
         return im       
         
@@ -97,20 +100,23 @@ class VAE(nn.Module):
                           im_wh=(64,64),
                           h_ch=32,
                           embed_len=32,
-                          batch_norm=False):
+                          batch_norm=False, **kwargs):
         super(VAE, self).__init__()
-        
+        self.embed_len = embed_len
         self.encoder = Encoder(in_ch=in_ch,
                               im_wh=im_wh,
                               h_ch=h_ch,
                               embed_len=embed_len,
-                              batch_norm=batch_norm,
-                              is_vae=True)
+                              batch_norm=batch_norm)
+        
+        self.logvar_fc = nn.Linear(in_features=self.encoder.enc_out_shape,
+                            out_features=self.embed_len)
+            
         self.decoder = Decoder(self.encoder)
     
     def reparametrize(self, mu, logvar):
         if self.training:
-            eps = torch.randn(*logvar.size()).to().to(mu.device)
+            eps = torch.randn(*logvar.size()).to(mu.device)
             std = torch.exp(0.5*logvar)
             z = mu + eps*std
         else:
@@ -118,27 +124,31 @@ class VAE(nn.Module):
         return z
         
     def forward(self, x):
-        mu, logvar = self.encoder(x)
+        mu = self.encoder(x)
+        logvar = self.logvar_fc(self.encoder.vec)
         z = self.reparametrize(mu, logvar)
         x_hat = self.decoder(z)
         return x_hat, mu, logvar
     
 
     def get_kl_rec(self,x,xr,mu,logvar):
-        kldiv = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar),dim=1)
-        rec = torch.sum((xr - x)**2,dim=(1,2,3))
-        return kldiv.mean(), rec.mean()
+        num_pixels = int(np.prod(x.size()[1:]))
+        kldiv = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar),dim=1) / num_pixels
+        rec = torch.sum((xr - x)**2,dim=(1,2,3)) / num_pixels
+        
+        return kldiv, rec
     
     def loss(self,x,xr,mu,logvar):
         kldiv, rec = self.get_kl_rec(x,xr,mu,logvar)
-        return rec + kldiv
+        loss = rec + kldiv
+        return loss.mean()
 
 class BetaVAE(VAE):
     def __init__(self,beta=1.,in_ch=3,
                           im_wh=(64,64),
                           h_ch=32,
                           embed_len=32,
-                          batch_norm=False):
+                          batch_norm=False, **kwargs):
         
         super(BetaVAE, self).__init__(in_ch, im_wh, h_ch, embed_len, batch_norm)
         self.beta = beta
@@ -146,4 +156,5 @@ class BetaVAE(VAE):
 
     def loss(self,x,xr,mu,logvar):
         kldiv, rec = self.get_kl_rec(x,xr,mu,logvar)
-        return rec + self.beta * kldiv
+        loss = rec + self.beta * kldiv
+        return loss.mean()
