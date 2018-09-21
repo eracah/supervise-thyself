a2d24c54a150a278f258f82e44b5f575e2131d48
eracah <ejracah@gmail.com>
2018-09-21 18:30:36 -0400

    changed to tests to take into account changes to collectors and iterators

--- tests.py
+++ tests.py
@@ -4,17 +4,12 @@
 # In[1]:
 
 
-from data.replay_buffer import BufferFiller
+#from data.replay_buffer import BufferFiller
 from data.iterators import UnusedPointsIterator, ListIterator, PolicyIterator
 from data.collectors import DataCollector
-from data.tr_val_test_splitter import setup_tr_val_val_test
+#from data.tr_val_test_splitter import setup_tr_val_val_test
 from utils import setup_env, convert_frame
 from functools import partial
-
-
-# In[2]:
-
-
 import unittest
 import sys
 import copy
@@ -25,69 +20,60 @@ import numpy as np
 import warnings
 import argparse
 
-
-# In[3]:
-
-
-class TestReplayBuffer(unittest.TestCase):
+# class TestReplayBuffer(unittest.TestCase):
     
-    def test_conflicting_buffer_fill(self):
-        bf = BufferFiller(env=gym.make(args.env_name))
-        rb = bf.fill(size=100)
+#     def test_conflicting_buffer_fill(self):
+#         bf = BufferFiller(env=gym.make(args.env_name))
+#         rb = bf.fill(size=100)
 
-        val_rb = bf.fill_with_unvisited_states(size=50,visited_buffer=rb)
+#         val_rb = bf.fill_with_unvisited_states(size=50,visited_buffer=rb)
 
-        rts = set(rb.get_zipped_list())
+#         rts = set(rb.get_zipped_list())
 
-        vts = set(val_rb.get_zipped_list())
+#         vts = set(val_rb.get_zipped_list())
 
-        tst_rb = bf.fill_with_unvisited_states(size=50,visited_buffer=rb+val_rb)
+#         tst_rb = bf.fill_with_unvisited_states(size=50,visited_buffer=rb+val_rb)
 
-        tst_rb.get_zipped_list()
-        tts = set(tst_rb.get_zipped_list())
+#         tst_rb.get_zipped_list()
+#         tts = set(tst_rb.get_zipped_list())
     
-        self.assertTrue(rts.isdisjoint(vts))
-        self.assertTrue(rts.isdisjoint(tts))
-        self.assertTrue(vts.isdisjoint(tts))
+#         self.assertTrue(rts.isdisjoint(vts))
+#         self.assertTrue(rts.isdisjoint(tts))
+#         self.assertTrue(vts.isdisjoint(tts))
 
-    def test_fill_buffer_with_rollouts(self):
-        bf = BufferFiller(env=gym.make(args.env_name))
-        size=100
-        rb = bf.fill(size)
-        self.assertEqual(len(rb), size)
+#     def test_fill_buffer_with_rollouts(self):
+#         bf = BufferFiller(env=gym.make(args.env_name))
+#         size=100
+#         rb = bf.fill(size)
+#         self.assertEqual(len(rb), size)
 
-    def test_fill_with_list(self):
-        bf = BufferFiller(env=gym.make(args.env_name))
+#     def test_fill_with_list(self):
+#         bf = BufferFiller(env=gym.make(args.env_name))
 
-        rb = bf.fill(size=100)
+#         rb = bf.fill(size=100)
 
-        rb_list = rb.get_zipped_list()
+#         rb_list = rb.get_zipped_list()
 
-        rb_copy = bf.fill_with_list(rb_list,size=-1)
+#         rb_copy = bf.fill_with_list(rb_list,size=-1)
 
-        rb_copy_list = rb_copy.get_zipped_list()
+#         rb_copy_list = rb_copy.get_zipped_list()
 
 
-        rbs = set(list(rb_list))
+#         rbs = set(list(rb_list))
 
-        rbcs = set(list(rb_copy_list))
-        self.assertEqual(rbs, rbcs)
+#         rbcs = set(list(rb_copy_list))
+#         self.assertEqual(rbs, rbcs)
         
-    def test_split_buffer(self):
-        prop = 0.7
-        bf = BufferFiller(env=gym.make(args.env_name))
+#     def test_split_buffer(self):
+#         prop = 0.7
+#         bf = BufferFiller(env=gym.make(args.env_name))
 
-        rb = bf.fill(size=200)
+#         rb = bf.fill(size=200)
 
-        len_unique = len(rb.get_zipped_list(unique=True))
-        rb1, rb2 = bf.split(rb,proportion=prop)
-        self.assertEqual(len(rb1),int(prop*len_unique))
+#         len_unique = len(rb.get_zipped_list(unique=True))
+#         rb1, rb2 = bf.split(rb,proportion=prop)
+#         self.assertEqual(len(rb1),int(prop*len_unique))
     
-
-
-# In[4]:
-
-
 class TestIterators(unittest.TestCase):
 
     def test_unused_points_iterator(self):
@@ -104,22 +90,22 @@ class TestIterators(unittest.TestCase):
         d = ch(dir_list,size=size)
         a = ch(act_list,size=size)
 
-        used = list(zip(x,y,d,a))
+        used = list(zip(x,y,d))
         ui = UnusedPointsIterator(used,env=env)
 
         unused = ui.get_unused_datapoints(used, env)
         unused = []
         for t in ui:
-            unused.append((t.x0_coord_x,t.x0_coord_y,t.x0_direction,t.a))
+            unused.append((t.x_coords,t.y_coords,t.directions))
         unused_set = set(unused)
         used_set = set(used)
         self.assertTrue(used_set.isdisjoint(set(unused_set)))
-        self.assertEqual(len(used_set.union(unused_set)),(env.grid_size - 2)**2 * num_dirs * num_actions)
+        self.assertEqual(len(used_set.union(unused_set)),(env.grid_size - 2)**2 * num_dirs)
 
 
     def test_policy_iterator(self):
 
-        pi = PolicyIterator(env=gym.make(args.env_name))
+        pi = PolicyIterator(env=gym.make(args.env_name),stop_at_done=True)
 
         # test continuing where you left off
         last_step = 0
@@ -150,25 +136,18 @@ class TestIterators(unittest.TestCase):
         d = ch(dir_list,size=size)
         a = ch(act_list,size=size)
 
-        list_of_points = list(zip(x,y,d,a))
+        list_of_points = list(zip(x,y,d))
         #print(list_of_points)
         list_it = ListIterator(list_of_points,env=gym.make(args.env_name))
         test_list = []
         for i,t in enumerate(list_it):
-            trans_tup = tuple([getattr(t,k) for k in ["x0_coord_x",
-                                                "x0_coord_y",
-                                                "x0_direction",
-                                                "a"] ])
+            trans_tup = tuple([getattr(t,k) for k in ["x_coords",
+                                                "y_coords",
+                                                "directions"] ])
 
             test_list.append(trans_tup)
         self.assertEqual(i, len(list_of_points) - 1)
         self.assertEqual(test_list, list_of_points)
-
-
-
-
-# In[5]:
-
 
 class TestCollectors(unittest.TestCase):
     def test_collect_specific_datapoint(self):
@@ -183,52 +162,59 @@ class TestCollectors(unittest.TestCase):
             x = ch(grid_list)
             y = ch(grid_list)
             d = ch(dir_list)
-            a = ch(act_list)
-            inp_tup = (x,y,d,a)
-            trans = dc.collect_specific_datapoint((x,y), d, a)
-            out_tup = tuple([getattr(trans,k) for k in ["x0_coord_x",
-                                                    "x0_coord_y",
-                                                    "x0_direction",
-                                                    "a"]])
+            #a = ch(act_list)
+            inp_tup = ([x],[y],[d])
+            trans = dc.collect_specific_datapoint((x,y), d)
+            out_tup = tuple([getattr(trans,k) for k in ["x_coords",
+                                                    "y_coords",
+                                                    "directions"]])
             self.assertEqual(inp_tup, out_tup)
 
-    def test_collect_data_point_per_policy(self):
+    def test_collect_transition_per_the_policy(self):
         dc = DataCollector(env=gym.make(args.env_name))
         for i in range(100):
-            trans = dc.collect_data_point_per_policy()
-            self.assertEqual(len(trans),11)
+            trans = dc.collect_transition_per_the_policy()
+            self.assertEqual(len(trans),7)
+            
+        
+        for i in range(2,50,1):
+            dc = DataCollector(env=gym.make(args.env_name),frames_per_trans=i)
+            trans = dc.collect_transition_per_the_policy()
+            self.assertEqual(len(trans.xs),i)
+            self.assertEqual(len(trans.x_coords),i)
+            self.assertEqual(len(trans.y_coords),i)
+            self.assertEqual(len(trans.directions),i)
+            self.assertEqual(len(trans.actions),i-1)
+            self.assertEqual(len(trans.rewards),i-1)
+            self.assertEqual(len(trans.dones),i-1)
 
-
-# In[6]:
-
-
-class TestDatasetReproducibility(unittest.TestCase):
-    def test_tr_set_match(self):
-        seed = 10
-        env, action_space, grid_size, num_directions,        tot_examples, random_policy = setup_env(args.env_name, seed = seed)
+# class TestDatasetReproducibility(unittest.TestCase):
+#     def test_tr_set_match(self):
+#         seed = 10
+#         env, action_space, grid_size, num_directions,        tot_examples, random_policy = setup_env(args.env_name, seed = seed)
         
-        convert_fxn = partial(convert_frame)
+#         convert_fxn = partial(convert_frame)
         
-        tr_buf1, _, _,_,_ = setup_tr_val_val_test(env, random_policy,
-                                                     convert_fxn, tot_examples, 
-                                                     batch_size=10, verbose=False)
+#         tr_buf1, _, _,_,_ = setup_tr_val_val_test(env, random_policy,
+#                                                      convert_fxn, tot_examples, 
+#                                                      batch_size=10, verbose=False)
         
-        env, action_space, grid_size, num_directions,        tot_examples, random_policy = setup_env(args.env_name, seed = seed)
+#         env, action_space, grid_size, num_directions,        tot_examples, random_policy = setup_env(args.env_name, seed = seed)
         
-        tr_buf2, _ = setup_tr_val_val_test(env, random_policy,
-                                                     convert_fxn, tot_examples, 
-                                                     batch_size=10, just_train=True, verbose=False)
+#         tr_buf2, _ = setup_tr_val_val_test(env, random_policy,
+#                                                      convert_fxn, tot_examples, 
+#                                                      batch_size=10, just_train=True, verbose=False)
         
-        self.assertEqual(set(tr_buf1.get_zipped_list()),set(tr_buf2.get_zipped_list()) )
+#         self.assertEqual(set(tr_buf1.get_zipped_list()),set(tr_buf2.get_zipped_list()) )
         
         
 
     
-    def test_all_sets_diff(self):
-        self.assertTrue(True)
+#     def test_all_sets_diff(self):
+#         self.assertTrue(True)
 
 
-# In[7]:
+# In[2]:
 
 
 if __name__ == "__main__":
@@ -238,7 +224,7 @@ if __name__ == "__main__":
         sys.argv = [""]
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--size",type=int, default=100),
+    parser.add_argument("--size",type=int, default=6),
     args = parser.parse_args()
     args.env_name = 'MiniGrid-Empty-%ix%i-v0'%(args.size,args.size)
     try:
