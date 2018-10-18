d735917c1333d767502ad3de5f69639d0941e79a
eracah <ejracah@gmail.com>
2018-10-17 20:13:02 -0400

    added more descriptive file path for models that differentiates label_name and eval_mode

--- utils.py
+++ utils.py
@@ -20,16 +20,11 @@ from itertools import product
 import random
 
 from comet_ml import Experiment
-def get_upper(st):
-    ret = "".join([s for s in st if s.isupper()])
-    return ret
 
-def parse_minigrid_env_name(name):
-    return name.split("-")[2].split("x")[0]
 
 def get_env_nickname(args):
-    env_nickname = get_upper(args.env_name) if "MiniGrid" in args.env_name else args.env_name.split("-")[0]
-    exp_nickname = str(args.resize_to[0]) + env_nickname + (parse_minigrid_env_name(args.env_name) if "MiniGrid" in args.env_name else "")
+    env_nickname = args.env_name.split("-")[0]
+    exp_nickname = str(args.resize_to[0]) + env_nickname
     return exp_nickname
 
 def get_hyp_str(args):
@@ -37,9 +32,20 @@ def get_hyp_str(args):
     if args.model_name == "beta_vae":
         hyp_str += ("beta=%f"%args.beta).rstrip('0').rstrip('.')
     return hyp_str 
+
+
 def get_child_dir(args, mode):
     env_nn = get_env_nickname(args)
-    child_dir = Path(mode) / Path(args.model_name) / Path(env_nn) / Path(("nb_" if args.test_notebook else "") + (get_hyp_str(args) if mode == "train" else args.label_name )  )
+    
+    
+    child_dir = Path(mode)
+    if mode == "eval" or mode == "test":
+        child_dir = child_dir/ Path(args.eval_mode) / Path(args.label_name)
+    
+    child_dir = child_dir / Path(args.model_name) / Path(env_nn) / Path(("nb_" if args.test_notebook else "") + "" if mode == "test" else get_hyp_str(args))
+    
+    
+    
     return child_dir
 
 
