e59cee224e5f70aedeb037c26a380acae3152d95
eracah <ejracah@gmail.com>
2018-10-18 21:15:15 -0400

    refactored env by env creation of get_latent_dict function

--- data/utils.py
+++ data/utils.py
@@ -7,51 +7,68 @@ import torch
 from functools import partial
 import math
 
+
+def atari_get_latent_dict(env):
+    env_name = env.env.spec.id
+    ram = env.env.ale.getRAM()
+    if env_name == 'PrivateEye-v0':
+        x_coord, y_coord = ram[63], ram[86]
+    elif env_name == 'Pitfall-v0':
+        x_coord, y_coord = ram[97], ram[105]
+    else:
+        assert False
+#     elif env_name == "MontezumaRevenge-v0":
+#         x_coord, y_coord = ram[42], ram[43]
+    latent_dict = dict(x_coord=x_coord,y_coord=y_coord)
+    return latent_dict
+
+def atari_get_nclasses_table(env):
+    env_name  = env.env.spec.id
+    if env_name == 'PrivateEye-v0':
+        num_x = 40
+        num_y = 20
+    elif env_name == 'Pitfall-v0':
+        num_x = 20
+        num_y = 20 
+    else:
+        assert False
+    nclasses_table = dict(x_coord=num_x, y_coord=num_y)
+    return nclasses_table
+
+
+def monster_kong_get_latent_dict(env):
+    grid_size = env.observation_space.shape[0]
+    player = env.env.game_state.game.newGame.Players[0]
+    (x_coord,y_coord), is_jumping, on_ladder = player.getPosition(), player.isJumping, player.onLadder
+    x_thresh, y_thresh =  math.floor(grid_size/env.num_coord_buckets), math.floor(grid_size/env.num_coord_buckets)
+    x_coord, y_coord =  math.floor(x_coord/x_thresh), math.floor(y_coord/y_thresh)
+
+    latent_dict = dict(x_coord=x_coord,y_coord=y_coord, is_jumping=is_jumping, on_ladder=on_ladder)
+    return latent_dict
+
+def monster_kong_get_nclasses_table(env):
+    num_coord_buckets = env.num_coord_buckets
+    nclasses_table = dict(x_coord=num_coord_buckets, y_coord=num_coord_buckets,is_jumping=2, on_ladder=2 )
+    return nclasses_table
+
+def catcher_get_latent_dict(env):
+    pass
+
 def setup_env(args):
     env = gym.make(args.env_name)
     env.seed(args.seed)
     num_coord_buckets = args.buckets
+    env.num_coord_buckets = num_coord_buckets
     action_space = list(range(env.action_space.n))
     if hasattr(env.env, "ale"):
-        def get_latent_dict(env):
-            env_name = env.env.spec.id
-            ram = env.env.ale.getRAM()
-            if env_name == 'PrivateEye-v0':
-                x_coord, y_coord = ram[63], ram[86]
-            elif env_name == 'Pitfall-v0':
-                x_coord, y_coord = ram[97], ram[105]
-            else:
-                assert False
-        #     elif env_name == "MontezumaRevenge-v0":
-        #         x_coord, y_coord = ram[42], ram[43]
-            latent_dict = dict(x_coord=x_coord,y_coord=y_coord)
-            return latent_dict
-        env_name = args.env_name
-        if env_name == 'PrivateEye-v0':
-            num_x = 40
-            num_y = 20
-        elif env_name == 'Pitfall-v0':
-            num_x = 20
-            num_y = 20 
-        else:
-            assert False
-        nclasses_table = dict(x_coord=num_x, y_coord=num_y)
+        get_latent_dict = atari_get_latent_dict
+        nclasses_table = atari_get_nclasses_table(env)
+    elif args.env_name in ['originalGame-v0','nosemantics-v0','noobject-v0','nosimilarity=v0','noaffordance-v0']:
+        get_latent_dict = monster_kong_get_latent_dict
+        nclasses_table = monster_kong_get_nclasses_table(env)
     else:
+        raise NotImplementedError
 
-        def get_latent_dict(env):
-            grid_size = env.observation_space.shape[0]
-            player = env.env.game_state.game.newGame.Players[0]
-            (x_coord,y_coord), is_jumping, on_ladder = player.getPosition(), player.isJumping, player.onLadder
-            x_thresh, y_thresh =  math.floor(grid_size/env.num_coord_buckets), math.floor(grid_size/env.num_coord_buckets)
-            x_coord, y_coord =  math.floor(x_coord/x_thresh), math.floor(y_coord/y_thresh)
-
-            latent_dict = dict(x_coord=x_coord,y_coord=y_coord, is_jumping=is_jumping, on_ladder=on_ladder)
-            return latent_dict
-
-
-        nclasses_table = dict(x_coord=num_coord_buckets, y_coord=num_coord_buckets,is_jumping=2, on_ladder=2 )
-        
-    env.num_coord_buckets = num_coord_buckets
     env.get_latent_dict = get_latent_dict
     env.nclasses_table = nclasses_table
     return env
