9b7cd4b36b4ebcb6b0befeb9590b42ac2bdc9964
eracah <ejracah@gmail.com>
2018-12-17 01:43:30 -0500

    added another linear layer to base encoder and l2 normalized embedding

--- models/base_encoder.py
+++ models/base_encoder.py
@@ -6,22 +6,25 @@ class Encoder(nn.Module):
     def __init__(self,
                  im_wh=(64,64),
                  in_ch=3,
-                 h_ch=32,
                  embed_len=32,
-                 base_enc_name="universe",
+                 fc_width = 512,
+                 base_enc_name="world_models",
                  **kwargs):
         
         super(Encoder,self).__init__()
         self.base_enc_name = base_enc_name
         self.im_wh = im_wh 
         self.in_ch = in_ch
-        self.h_ch = h_ch
+        self.fc_width = fc_width
         self.embed_len = embed_len
         self.vec = None
-        self.encoder = get_encoder(self.base_enc_name, in_ch, h_ch)
-                
-        self.fc = nn.Linear(in_features=self.enc_out_shape,
-                            out_features=self.embed_len)
+        self.encoder = get_encoder(self.base_enc_name, in_ch)
+       
+        self.fcs = nn.Sequential(nn.Linear(in_features=self.enc_out_shape,
+                                           out_features=self.fc_width),
+                                 nn.Linear(in_features=self.fc_width,
+                                           out_features=self.embed_len)
+                                )
 
 
 
@@ -39,11 +42,13 @@ class Encoder(nn.Module):
     def forward(self,x):
         fmaps = self.encoder(x)
         self.vec = fmaps.view(fmaps.size(0),-1)
-        embedding = self.fc(self.vec)
+        raw_embedding = self.fcs(self.vec)
+        # normalize embedding by its L2 norm
+        embedding = raw_embedding / raw_embedding.norm(p=2,dim=1, keepdim=True)
         return embedding
 
     
-def get_encoder(name, in_ch, h_ch):
+def get_encoder(name, in_ch):
     if name == "world_models":
         enc = nn.Sequential(
             nn.Conv2d(in_channels=in_ch,out_channels=32,kernel_size=4, stride=2),
@@ -55,6 +60,7 @@ def get_encoder(name, in_ch, h_ch):
             nn.Conv2d(in_channels=128,out_channels=256,kernel_size=4, stride=2),
             nn.ReLU())
     elif name == "universe":
+        h_ch =32
         enc = nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channels=h_ch,
                       kernel_size=3, stride=2, padding=1),
             nn.ELU(),
