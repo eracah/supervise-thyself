378158ce1b4b56e2d6e58239b7394c327573a19c
eracah <ejracah@gmail.com>
2019-02-24 21:24:03 -0500

    got visialization working for seqience of frames

--- evaluations/fmap_superimpose.py
+++ evaluations/fmap_superimpose.py
@@ -26,3 +26,31 @@ def superimpose_fmaps(encoder, test_set, experiment):
         experiment.log_figure(figure_name="it%i_ind%i"%(i,ind),figure=fig)
         if i > 8:
             break
+            
+            
+
+def superimpose_seq_frames(encoder, test_set, fmap_index):
+
+        for i,trans in enumerate(test_set):#= next(test_set.__iter__())
+
+            x_example = trans.xs[1][:-1:3]
+
+            fmap_model = list(encoder.children())[0]
+
+            fmap = fmap_model(x_example)
+
+            fmap_i = nn.functional.interpolate(fmap[:,fmap_index,None],size=x_example.shape[-2:],mode="bilinear")
+
+            fm_grid = make_grid(fmap_i,nrow=10,padding=0)[0].detach().numpy()
+
+            #plt.imshow(fm_grid)
+
+            xgrid = make_grid(x_example,nrow=10,padding=0)[0].detach().numpy()
+
+            fig = plt.figure(i,frameon=False,figsize=(50,50))
+            plt.clf()
+            im1 = plt.imshow(xgrid)
+            im2 = plt.imshow(fm_grid, cmap=plt.cm.viridis, alpha=0.7)
+            #experiment.log_figure(figure_name="it%i_ind%i"%(i,ind),figure=fig)
+            if i > 2:
+                break
